616830e5616ace165a9cadf612e1aaeeb6cf071a
2young <yh20studio@gmail.com>
2021-09-17 14:58:41 +0900

    Add 최솟값과최댓값.py
    baekJoon, Lv.gold, Segment Tree

--- README.md
+++ README.md
@@ -10,6 +10,7 @@
 |합분해|[문제 링크](https://www.acmicpc.net/problem/2225)|[문제 풀이](https://github.com/yh20studio/Algorithm-problem-solving/blob/master/baekjoon/lv.gold/합분해.py)|다이나믹프로그래밍|
 |행렬곱셈순서|[문제 링크](https://www.acmicpc.net/problem/11049)|[문제 풀이](https://github.com/yh20studio/Algorithm-problem-solving/blob/master/baekjoon/lv.gold/행렬곱셈순서.py)|다이나믹프로그래밍|
 |외판원순회|[문제 링크](https://www.acmicpc.net/problem/2098)|[문제 풀이](https://github.com/yh20studio/Algorithm-problem-solving/blob/master/baekjoon/lv.gold/외판원순회.py)|다이나믹프로그래밍, 비트마스킹|
+|최솟값과최댓값|[문제 링크](https://www.acmicpc.net/problem/2357)|[문제 풀이](https://github.com/yh20studio/Algorithm-problem-solving/blob/master/baekjoon/lv.gold/최솟값과최댓값.py)|세그먼트 트리|
 </div>
 
 </details>
---
+++ baekjoon/lv.gold/최솟값과최댓값.py
@@ -0,0 +1,65 @@
+# link : https://www.acmicpc.net/problem/2357
+# Lv : gold
+# Category : 세그먼트 트리
+
+# 주어진 순서쌍을 이용해서 해당 범위내에서 최대, 최솟값을 찾아야 함.
+# 순서쌍이 주어질 때마다 최대, 최솟값을 계산한다면 시간이 오래걸릴 것이므로, 계산한 값을 저장해서 사용하는 DP를 이용하려고 했으나,
+# N의 값이 커서 메모리 초과현상이 일어났다. 따라서 세그먼트 트리를 사용하기로 함.
+
+import sys  
+
+def init(start, end, index):
+  if tree[index] != [0, 0]:
+    return tree[index]
+  
+  if start == end:
+    tree[index] = [numbers[start], numbers[start]]
+    return tree[index]
+  
+  mid = (start + end)//2
+  a, b = init(start, mid, index*2)
+  c, d = init(mid+1, end, index*2 + 1)
+  if a > c:
+    a = c
+  if b < d:
+    b = d
+  tree[index] = [a, b]
+
+  return tree[index]
+
+def query(start, end, index, qLeft, qRight):
+  
+  if start > qRight or end < qLeft:
+    return [1000000001, 0]
+
+  if start >= qLeft and end <= qRight:
+    return tree[index]
+    
+  mid = (start + end)//2
+
+  a, b = query(start, mid, index*2, qLeft, qRight)
+  c, d = query(mid+1, end, index*2+1, qLeft, qRight)
+
+  return [min(a, c), max(b, d)]
+
+    
+answer = []
+N, M = map(int, (sys.stdin.readline().split()))
+# 순서쌍에서 사용되는 index는 1부터 시작하므로 numbers에도 데이터를 1 부터 받기 위함.
+numbers = [0]
+orders = []  
+
+for _ in range(N):
+  numbers.append(int(sys.stdin.readline()))
+
+for _ in range(M):
+  orders.append(list(map(int, (sys.stdin.readline().split()))))
+
+tree =[[0, 0] for _ in range((N*4))]
+init(1, N, 1)
+
+for a, b in orders:
+  minimum, maximum = query(1, N, 1, a, b)
+  print(f"{minimum} {maximum}")
+
+
